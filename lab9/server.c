cf943245d70f6807b4906206e9d08d3a4cd2fb4a
Shadi Zidany <sza229@sfu.ca>
2025-11-14 18:29:24 -0800

    Lab9 work

---
+++ lab9/server.c
@@ -0,0 +1,91 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUF_SIZE 64
+#define PORT 8000
+#define LISTEN_BACKLOG 32
+
+#define handle_error(msg)                                                      \
+  do {                                                                         \
+    perror(msg);                                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } while (0)
+
+// Shared counters for: total # messages, and counter of clients (used for
+// assigning client IDs)
+int total_message_count = 0;
+int client_id_counter = 1;
+
+// Mutexs to protect above global state.
+pthread_mutex_t count_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t client_id_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct client_info {
+  int cfd;
+  int client_id;
+};
+
+void *handle_client(void *arg) {
+  struct client_info *client = (struct client_info *)arg;
+  printf("New client created! ID %d on socket FD %d\n", client->client_id,
+         client->cfd);
+  ssize_t num_read;
+  char buf[BUF_SIZE];
+  while ((num_read = read(client->cfd, buf, BUF_SIZE - 1)) > 0) {
+    buf[num_read] = 0;
+    pthread_mutex_lock(&count_mutex);
+    total_message_count++;
+    pthread_mutex_unlock(&count_mutex);
+    printf("Msg #   %d: Client ID %d: %s", total_message_count,
+           client->client_id, buf);
+  }
+  close(client->cfd);
+  printf("Ending thread for client %d\n", client->client_id);
+  free(client);
+  return NULL;
+}
+
+int main() {
+  struct sockaddr_in addr;
+  int sfd;
+
+  sfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sfd == -1) {
+    handle_error("socket");
+  }
+
+  memset(&addr, 0, sizeof(struct sockaddr_in));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(PORT);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in)) == -1) {
+    handle_error("bind");
+  }
+
+  if (listen(sfd, LISTEN_BACKLOG) == -1) {
+    handle_error("listen");
+  }
+
+  for (;;) {
+    int cfd = accept(sfd, NULL, NULL);
+    pthread_t thread;
+    struct client_info *client = malloc(sizeof(struct client_info));
+    client->cfd = cfd;
+    client->client_id = client_id_counter++;
+    pthread_create(&thread, NULL, handle_client, client);
+    pthread_detach(thread);
+  }
+
+  if (close(sfd) == -1) {
+    handle_error("close");
+  }
+
+  return 0;
+}
